4df6b7cb2a8c2a74f5150d353bab07e3bd998772
TIGERKING00 <104628282+TIGERKING00@users.noreply.github.com>
2022-07-13 13:57:02 +0530

    Add files via upload

---
+++ Do.java
@@ -0,0 +1,22 @@
+class Loop2
+{
+public static void main(String[] args)
+{
+int i=10,j;
+System.out.println("Descending Order");
+do
+{
+System.out.println(i);
+i--;
+}
+while(i>3);
+System.out.println("Descending Order");
+for(j=10;j>6;j--)
+{
+System.out.println(j);
+}
+
+
+}
+
+}
