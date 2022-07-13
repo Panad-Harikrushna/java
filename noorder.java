2aebfa2a1756d18ff83dc26a8e7e8f0b405996f1
TIGERKING00 <104628282+TIGERKING00@users.noreply.github.com>
2022-07-13 13:58:30 +0530

    Add files via upload

---
+++ noorder.java
@@ -0,0 +1,19 @@
+class Loop
+{
+public  static void main(String[] args)
+{
+int i;
+System.out.println("ascending order");
+for(i=1;i<=10;i++)
+{
+System.out.println(i);
+}
+
+System.out.println("descending order");
+
+for(i=10;i>=1;i--)
+{
+System.out.println(i);
+}
+}
+}
