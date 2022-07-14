ac6ddfa4da3c6626aa7cb298d5462c58f3a1cdf7
TIGERKING00 <104628282+TIGERKING00@users.noreply.github.com>
2022-07-14 13:12:21 +0530

    Update printno.java

--- printno.java
+++ printno.java
@@ -1,3 +1,11 @@
+/*4.Prints the numbers from 1 to 100 such that:
+
+If the number is a multiple of 3, you need to print "Deep" instead of that number.
+If the number is a multiple of 5, you need to print "Tech" instead of that number.
+If the number is a multiple of both 3 and 5, you need to print "DeepTech" instead of that number.*/
+
+
+
 import java.util.*;
 
 class PosNeg
