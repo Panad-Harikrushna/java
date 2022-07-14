23dbc1d30d69beef9c7dc9883ffa0d4316643b6d
TIGERKING00 <104628282+TIGERKING00@users.noreply.github.com>
2022-07-14 13:01:36 +0530

    Add files via upload

---
+++ positivenegative.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class PosNeg
+{
+    public static void main(String[] args)
+	{
+	int n;
+	Scanner in=new Scanner(System.in);
+	System.out.println(" Enter a integer   :");
+	n=in.nextInt();
+	System.out.println(" Entered number is :"+n);
+		if(n%2==0)
+		{
+		System.out.println("Entered number is positive :"+n);
+		}
+		else 
+		 {
+		System.out.println("Entered number is Negative :"+n);	
+		   }
+ 	   }		
+}
+
+
+
