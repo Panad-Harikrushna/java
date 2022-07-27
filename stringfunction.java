b156060421b57771699e4da76a844e974ef7e85f
TIGERKING00 <104628282+TIGERKING00@users.noreply.github.com>
2022-07-27 11:29:08 +0530

    Add files via upload

---
+++ stringfunction.java
@@ -0,0 +1,15 @@
+class Stringlength
+{
+  public static void main(String[]  args)
+  {
+     	 String name=("Hello");
+    	System.out.println(name.length());
+	System.out.println(name.charAt(2));
+	System.out.println(name.toLowerCase());
+	System.out.println(name.toUpperCase());
+	System.out.println(name.indexOf("l"));
+	System.out.println(name.substring(0,4));
+	String name1=("Hii");
+	
+ }
+}
