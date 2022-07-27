fbf685e4477bd0a90c76fc74bda37c301b98d0bd
TIGERKING00 <104628282+TIGERKING00@users.noreply.github.com>
2022-07-27 11:59:36 +0530

    Update stringfunction.java

--- stringfunction.java
+++ stringfunction.java
@@ -3,18 +3,18 @@ class Stringlength
   public static void main(String[]  args)
   {
       String name=("Hello");
-    	System.out.println(name.length());
-	System.out.println(name.charAt(2));
-	System.out.println(name.toLowerCase());
-	System.out.println(name.toUpperCase());
-	System.out.println(name.indexOf("l"));
-	System.out.println(name.substring(0,4));
-	System.out.println(name.replace('H','C'));
+    	System.out.println("Length function : "+name.length());
+	System.out.println("Character at function : "+name.charAt(2));
+	System.out.println("To lower case function : "+name.toLowerCase());
+	System.out.println("To upper case function : "+name.toUpperCase());
+	System.out.println("ndex function : "+name.indexOf("l"));
+	System.out.println("substring function : "+name.substring(0,4));
+	System.out.println("Replace function : "+name.replace('H','C'));
       String name1="       Anudip";
-	System.out.println(name1.trim());
-	System.out.println(name.equals("hello"));
-	System.out.println(name.equalsIgnoreCase("hello"));
-	System.out.println(name.startsWith("e"));
+	System.out.println("Trim function : "+name1.trim());
+	System.out.println("Equal function : "+name.equals("hello"));
+	System.out.println("Equal and ignor case function : "+name.equalsIgnoreCase("hello"));
+	System.out.println("Starts with function : "+name.startsWith("ja"));
 	
  }
 }
