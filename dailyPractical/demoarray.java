2fbd3eba8fbbd1547e3614fd6cc27a0346df1f1e
TIGERKING00 <104628282+TIGERKING00@users.noreply.github.com>
2022-07-28 12:38:26 +0530

    Add files via upload

---
+++ dailyPractical/demoarray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class SingleDimensionDemo
+{
+	
+public static void main(String aw[])
+	{	
+		
+             	int mark[]={25,89,100};
+		Scanner s=new Scanner(System.in);
+		for(int i=0;i<3;i++)
+		    {
+			System.out.println(mark[i]);	
+             		    }
+		
+	}
+}
